b84714b9854a83c2b9d04f7a9b9edfdbf1d06074
zjjt <zehijean1988@gmail.com>
2020-05-08 10:32:50 +0000

    ok

---
+++ userService/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	db, err := createPostgresDBConnection()
+	defer db.Close()
+	if err != nil {
+		log.Fatalf("Could not connect to the database: %v", err)
+	} else {
+		log.Println("Connected to DB successfully")
+	}
+}
