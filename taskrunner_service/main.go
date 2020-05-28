a1520294ccf91ffc898ec2dbe0862b535d3cf4b7
zjjt <zehijean1988@gmail.com>
2020-05-28 11:54:20 +0000

    ok

--- taskrunner_service/main.go
+++ taskrunner_service/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/joho/godotenv"
 	"github.com/micro/go-micro/v2"
@@ -17,6 +19,7 @@ func publishEvent(pubsub broker.Broker, topic string) error {
 	//create a broker message
 	msg := &broker.Message{}
 	//publish the message to the broker
+	log.Println("[PUB] publishing event ", topic)
 	if err := pubsub.Publish(topic, msg); err != nil {
 		theerror := fmt.Sprintf("%v --from taskrunner_service", err)
 		log.Printf("[PUB] failed %s\n", theerror)
@@ -35,23 +38,22 @@ func main() {
 	service := micro.NewService(micro.Name("abjnet.service.taskrunner"))
 	service.Init()
 	//get the broker instance
-	if err := broker.Init(); err != nil {
+	pubsub := service.Server().Options().Broker
+	if err := pubsub.Init(); err != nil {
 		log.Fatalf("Broker Init error: %v", err)
 	}
-	if err := broker.Connect(); err != nil {
+	if err := pubsub.Connect(); err != nil {
 		log.Fatal(err)
 	}
 
 	//publishing the event and sending all the subs to the email_service
 	job := cron.New()
 	job.AddFunc(os.Getenv("MAJSUBAT"), func() {
-		log.Println("[PUB]publishing the update subscriptions event ")
 		if err := publishEvent(pubsub, topic[1]); err != nil {
 			fmt.Println(err)
 		}
 	})
 	job.AddFunc(os.Getenv("DELETESUBSAT"), func() {
-		log.Println("[PUB]publishing the delete subscriptions event ")
 		if err := publishEvent(pubsub, topic[0]); err != nil {
 			fmt.Println(err)
 		}
@@ -61,8 +63,15 @@ func main() {
 		theerror := fmt.Sprintf("%v --from taskrunner_service", err)
 		fmt.Println(theerror)
 	}
-	//to stop the goroutine from exiting
+	//to stop the goroutine from exiting unless we receive interrupt from
+	c := make(chan os.Signal, 1) // we need to reserve to buffer size 1, so the notifier are not blocked
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		log.Println("exiting now...")
+		<-c
+		os.Exit(0)
+	}()
 	for {
-		select {}
 	}
+
 }
